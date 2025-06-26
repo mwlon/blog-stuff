precision fix
Signed-off-by: mwlon <m.w.loncaric@gmail.com>

projection/math_utils.py
@@ -118,8 +118,10 @@ def calc_euc(sph):
   return np.stack([x, y, z], axis=1)
 
 def rotate(xy, rot):
-  rot_mat = np.array([[np.cos(rot), -np.sin(rot)], [np.sin(rot), np.cos(rot)]])
-  return (rot_mat @ xy.transpose()).transpose()
+  # without highest matmul precision, we can easily get nan loss on CUDA
+  jax.config.update("jax_default_matmul_precision", "highest")
+  rot_mat = jnp.array([[np.cos(rot), np.sin(rot)], [-np.sin(rot), np.cos(rot)]])
+  return xy @ rot_mat
 
 def calc_distortion_dets(distortion):
   assert len(distortion.shape) == 3

projection/serialization.py
@@ -4,6 +4,7 @@ import numpy as np
 from dataclasses import dataclass
 import numpy as np
 from lattice import Lattice
+import math_utils
 
 def save(name, sph, triangles, xy):
   d = f'results/{name}'
@@ -26,13 +27,7 @@ class LoadedData:
   xy: np.ndarray
 
   def lattice(self) -> Lattice:
-    sph = self.sph
-    theta = sph[:, 0]
-    phi = sph[:, 1]
-    x = np.sin(phi) * np.cos(theta)
-    y = np.sin(phi) * np.sin(theta)
-    z = np.cos(phi)
-    euc = np.stack([x, y, z], axis=1)
+    euc = math_utils.calc_euc(sph)
     return Lattice(sph=sph, euc=euc, triangles=self.triangles)
   
 def load(name):